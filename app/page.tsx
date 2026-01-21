29e566ccf2bc4b614685e3d4d1f12c2988e00e41
KN99HN <khanhnguyen99hn@gmail.com>
2026-01-21 00:40:09 -0500

    Basic table with fake data from transiter github repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,71 @@
-import Image from "next/image";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
-export default function Home() {
+type Train = {
+ name: string,
+ arrivalTime: number,
+ headsign: string,
+}
+
+function computeDelta(t1: Date, t2: Date) : number {
+  const delta = (t1 - t2) / (1000 * 60);
+  if (delta < 0) {
+   return 1;
+  }
+  return Math.ceil(delta);
+}
+
+async function processTrainData(trains) : Array<Train> {
+  const stopTimes = trains.stopTimes;
+  const unique_trains = [];
+  return stopTimes.map((stop) => {
+    const trainId = stop.trip.route.id;
+    const arrivalTime = computeDelta(new Date(parseInt(stop.arrival.time) * 1000), new Date()); 
+    if(arrivalTime > 30) {
+      return undefined;
+    }
+    return { value: {
+     name: trainId,
+     arrivalTime: arrivalTime,
+     headsign: stop.headsign,
+    }};
+  }).filter((stop) => stop !== undefined).map(x => x.value);
+} 
+
+
+ 
+export default async function Home() {
+  const data = await fetch('https://demo.transiter.dev/systems/us-ny-subway/stops/D15?include=stop_times');
+  const json = await data.json();
+  const trains: Array<Train> = await processTrainData(json);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+<Table>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Train</TableHead>
+      <TableHead>Direction</TableHead>
+      <TableHead>Status</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    {trains.map((train) => (
+      <TableRow key={train.name}>
+        <TableCell className="font-base">{train.name}</TableCell>
+        <TableCell>{train.headsign}</TableCell>
+        <TableCell>{train.arrivalTime} Minutes</TableCell>
+      </TableRow>
+    ))}
+  </TableBody>
+</Table>
+
   );
 }
